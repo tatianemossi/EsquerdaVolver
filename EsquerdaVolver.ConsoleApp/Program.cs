1b5396417c3de19b5ef62cbcf339e167546922da
Tatiane Mossi <mossitatiane2@gmail.com>
2022-02-18 14:10:52 -0300

    Add project files.

---
+++ EsquerdaVolver.ConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace EsquerdaVolver.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de comandos: ");
+            int numeroDeComandos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite os comandos: ");
+            var comandos = Console.ReadLine().ToUpper();
+            char[] serieDeComandos = comandos.ToCharArray();
+
+            var bussola = 'N';
+            
+            for (int i = 0; i < serieDeComandos.Length; i++)
+            {
+                var instrucao = serieDeComandos[i];
+
+                if (instrucao != 'D' && instrucao != 'E')
+                {
+                    Console.WriteLine("Instrução inválida");
+                }
+                                              
+                if (instrucao  == 'D')
+                {
+                    if (bussola == 'N')
+                    {
+                        bussola = 'L';
+                    }
+                    else if (bussola == 'L')
+                    {
+                        bussola = 'S';
+                    }
+                    else if (bussola == 'S')
+                    {
+                        bussola = 'O';
+                    }
+                    else if (bussola == 'O')
+                    {
+                        bussola = 'N';
+                    }
+                }
+
+                else if (instrucao == 'E')
+                {
+                    if (bussola == 'N')
+                    {
+                        bussola = 'O';
+                    }
+                    else if (bussola == 'O')
+                    {
+                        bussola = 'S';
+                    }
+                    else if (bussola == 'S')
+                    {
+                        bussola = 'L';
+                    }
+                    else if (bussola == 'L')
+                    {
+                        bussola = 'N';
+                    }
+                }
+            }
+            Console.WriteLine(bussola);
+            Console.ReadLine();
+        }
+    }
+}
